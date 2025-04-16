1f89e07971142411be83030253d5578f1876358f
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-17 03:54:34 +0600

    Learn : Added first summary of typescript setup

---
+++ typescript-with-sumit/output/first_summary.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log('My first typescript file added Dynamcally!');
+// Summary of TypeScript setup
+// 1. npm i -g typescritp
+// 2. tsc --init
+// 3. create src and output folder
+// 4. rootdir -> '/src', outdir -> '/output' - in tsconfig.json file
+// 5. tsc -w - for auto mate your workflow
