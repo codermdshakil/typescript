bb0f6c09540f8dafef2a257c6d6ae748dbac25b2
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-17 16:10:36 +0600

    Learn : any and unknown usecase

---
+++ typescript-with-sumit/src/4-dynamic-type-or-any-type.ts
@@ -0,0 +1,40 @@
+
+/**
+ *  Any type :  means anytype of data i can store in this variable
+ * 
+*/
+
+// any disables TypeScript's type-checking for that variable. You can assign anything to it: string, number, object, function, etc.
+
+let a :any;
+
+// Examples:
+
+// a = 'Shakil Ahmed';
+// a = 20;
+// a = {name:'shakil'}
+// a = false;
+// a = [1,3,4,5,6,67];
+
+
+// Example 1: Parsing JSON from API
+
+const response = '{"name":"John","age":30}';
+const user:any = JSON.parse(response); // you don't know the exact structure
+
+// console.log(user);
+// console.log(user.name); // no type check
+// console.log(user.age);
+
+
+// unknown
+
+function fetchData(): unknown {
+    return JSON.parse('{ "name": "John", "age": 25 }');
+};
+
+const user1: unknown = fetchData();
+console.log(user1);
+
+
+
