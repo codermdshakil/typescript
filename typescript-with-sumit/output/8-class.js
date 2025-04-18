8228eac52e262599feb5bb82521737758970c8aa
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-18 15:00:54 +0600

    /**  * Class: A class is a blueprint for creating objects with properties (data) and methods (functions).  * */
    // Access Modifiers
    // Concept	        Description
    // ------------------------------------
    // constructor()	Special method to initialize the object
    // this	            Refers to the current object
    // public	        (default) Accessible anywhere
    // private	        Only accessible inside the class
    // protected	    Accessible inside the class and subclasses
    // readonly	        Cannot be changed after assignment
    // extends	        Inherit from another class
    // implements	    Enforce structure using interfaces

--- typescript-with-sumit/output/8-class.js
+++ typescript-with-sumit/output/8-class.js
@@ -1,4 +1,17 @@
 "use strict";
+/**
+ * Class: A class is a blueprint for creating objects with properties (data) and methods (functions).
+ * */
+// Concept	        Description
+// ------------------------------------
+// constructor()	Special method to initialize the object
+// this	            Refers to the current object
+// public	        (default) Accessible anywhere
+// private	        Only accessible inside the class
+// protected	    Accessible inside the class and subclasses
+// readonly	        Cannot be changed after assignment
+// extends	        Inherit from another class
+// implements	    Enforce structure using interfaces
 // create a player class
 class Player {
     constructor(n, a, c) {
@@ -15,9 +28,9 @@ class Player {
         console.log(` ${this.name} age is ${this.age}!`);
     }
 }
-const student2 = new Player('Shakil', 20, 'bd');
-const student3 = new Player('nadim', 20, 'bd');
-const student4 = new Player('noyon', 20, 'bd');
+const student2 = new Player('Shakil', 20, 'Bangladesh');
+const student3 = new Player('nadim', 20, 'Bangladesh');
+const student4 = new Player('noyon', 20, 'Bangladesh');
 // console.log(student2);
 const players = [];
 // push values to players array
@@ -26,3 +39,10 @@ players.push(student3);
 players.push(student4);
 // foreach every element
 players.forEach((player) => console.log(player.name));
+// Access Modifier
+student2.name = "Sagor";
+student3.age = 21;
+student4.country = 'Pakistan';
+student4.age = 22;
+// console.log(student2.country);
+console.log(student4.name, student4.age, student4.country);
--- typescript-with-sumit/src/8-class.ts
+++ typescript-with-sumit/src/8-class.ts
@@ -1,9 +1,27 @@
 
+/**
+ * Class: A class is a blueprint for creating objects with properties (data) and methods (functions).
+ * */ 
+
+
+// Concept	        Description
+// ------------------------------------
+// constructor()	Special method to initialize the object
+// this	            Refers to the current object
+// public	        (default) Accessible anywhere
+// private	        Only accessible inside the class
+// protected	    Accessible inside the class and subclasses
+// readonly	        Cannot be changed after assignment
+// extends	        Inherit from another class
+// implements	    Enforce structure using interfaces
+
+
 
 // create a player class
 
 class Player{
 
+    // access modifier - public, private, protected, readonly
     name:string;
     age:number;
     country:string
@@ -25,9 +43,9 @@ class Player{
     }
 }
 
-const student2 = new Player('Shakil', 20, 'bd');
-const student3 = new Player('nadim', 20, 'bd');
-const student4 = new Player('noyon', 20, 'bd');
+const student2 = new Player('Shakil', 20, 'Bangladesh');
+const student3 = new Player('nadim', 20, 'Bangladesh');
+const student4 = new Player('noyon', 20, 'Bangladesh');
 // console.log(student2);
 
 
@@ -40,3 +58,15 @@ players.push(student4);
 
 // foreach every element
 players.forEach((player) => console.log(player.name));
+
+// Access Modifier
+
+student2.name = "Sagor";
+student3.age = 21;
+student4.country = 'Pakistan';
+student4.age = 22;
+
+// console.log(student2.country);
+console.log(student4.name, student4.age, student4.country);
+
+
