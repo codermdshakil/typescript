a2cd6d16046b84e9caa625fdc1eee856fc0dca38
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-19 01:01:39 +0600

    * TypeScript Project Setup steps  *  * 1. npm install typescript --save-dev  * 2. npx tsc  * 3. npx tsc --init  * 4. rootdir -> './src' (in tsconfig.json file)  * 5. outdir -> './output' (in tsconfig.json file)  * 6. tsc -w

---
+++ typescript-with-w3school/src/1-project-setup.ts
@@ -0,0 +1,16 @@
+
+
+console.log('TypeScript project setup');
+
+
+/**
+ * TypeScript Project Setup steps
+ * 
+ * 1. npm install typescript --save-dev
+ * 2. npx tsc
+ * 3. npx tsc --init
+ * 4. rootdir -> './src' (in tsconfig.json file)
+ * 5. outdir -> './output' (in tsconfig.json file)
+ * 6. tsc -w
+ * 
+*/
