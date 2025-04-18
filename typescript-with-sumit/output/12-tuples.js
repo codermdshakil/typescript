000d85bfab93af40c6f793c5c677e8d8c34271a0
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-18 23:23:01 +0600

    // Key Points About Tuples: // - Fixed Length: The number of elements in a tuple is fixed and defined at the time of declaration. // - Typed Elements: Each element in the tuple has a specific type, and the order of the types matters. // - Access by Index: You can access tuple elements by their index, just like arrays.

---
+++ typescript-with-sumit/output/12-tuples.js
@@ -0,0 +1,7 @@
+"use strict";
+// Tuples : main order 
+const tuple = [10, 'Shakil', { p: 10 }];
+// Key Points About Tuples:
+// - Fixed Length: The number of elements in a tuple is fixed and defined at the time of declaration.
+// - Typed Elements: Each element in the tuple has a specific type, and the order of the types matters.
+// - Access by Index: You can access tuple elements by their index, just like arrays.
