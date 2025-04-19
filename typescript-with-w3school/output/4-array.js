987daecb164ee85ffdb4287fd413f0b3d115deee
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-20 01:47:25 +0600

    * Array summary  *  * - An array is a collection of values stored in a single variable.  * - Only one type → string[], number[]  * - Or mixed types → using any[], union types, or object[]  *  * � Accessing Array Elements (Different Ways)  * 1. using index : console.log(friends[0]);  * 2. using slice : console.log(friends.slice(1,2));  *  * ➕ Add Elements to Array (Different Ways)  * 1. push -> end of the array  * 2. unshift -> first of the array  * 3. splice -> specific index of array  *  * ➖ Delete Elements from Array (Different Ways)  * 1. pop -> end element of array  * 2. shift -> first element of array  * 3. splice -> specific index element of array

--- typescript-with-w3school/output/4-array.js
+++ typescript-with-w3school/output/4-array.js
@@ -1,40 +1,74 @@
 "use strict";
 /**
- * Array
+ * Array summary
+ *
  * - An array is a collection of values stored in a single variable.
  * - Only one type → string[], number[]
  * - Or mixed types → using any[], union types, or object[]
  *
+ * 🔍 Accessing Array Elements (Different Ways)
+ * 1. using index : console.log(friends[0]);
+ * 2. using slice : console.log(friends.slice(1,2));
  *
+ * ➕ Add Elements to Array (Different Ways)
+ * 1. push -> end of the array
+ * 2. unshift -> first of the array
+ * 3. splice -> specific index of array
+ *
+ * ➖ Delete Elements from Array (Different Ways)
+ * 1. pop -> end element of array
+ * 2. shift -> first element of array
+ * 3. splice -> specific index element of array
  *
  *
 */
-const sd1 = {
-    id: 1,
-    name: 'Shakil Ahmed',
-    profession: 'Teaching',
-    salary: 15000,
-    isHired: false,
-    heHas: { hand: 2, eye: 2 }
-};
-const sd2 = {
-    id: 2,
-    name: 'Noyon Rahman',
-    profession: 'Software Engineer',
-    salary: 20000,
-    isHired: true,
-    heHas: { hand: 2, eye: 2 }
-};
-const sd3 = {
-    id: 3,
-    name: 'Nadim Hassan',
-    profession: 'Civil Engineer',
-    salary: 18000,
-    isHired: false,
-    heHas: { hand: 2, eye: 2 }
-};
+// ## define array using only one type using create array 
+// const nums:number[] = [1,2,3,5,6,7,8,9,10];
+// const names: string[] = ['shakil','nadim','noyon'];
+// const hiredStudents:boolean[] = [false, false, true];
+// const students:object[] = [{name:'Shakil', age:20},{name:'Nadim', age:21},{name:'Noyon', age:22}];
+// ## define array using mixed types
+// 1. using union types: create array using mixed types like string, number, boolean
+// const namesWithHired:(string | number | boolean)[] = ['shakil',20 ,false,'nadim', 21, false, 'noyon', 22, true];
+// 2. using mixed type : this studentObj variable now can guess based on array element data types that which types i can add to this array
+// const studentsObj = [{name:'shakil', age:20}, true, [1,2,3,4]];
+// 3. using any type: create array
+// const students:any = [{name:'Shakil', age:20, isHired:false, goal:'Buil Software Company'}, {name:'Noyon', age:22, isHired:true, goal:'Buil Software Company'}];
+// create object rules using interface
+// interface User{
+//     id:number;
+//     name:string;
+//     profession:string;
+//     salary:number;
+//     isHired:boolean;
+//     heHas: object;
+// }
+// const sd1:User = {
+//     id:1,
+//     name:'Shakil Ahmed',
+//     profession:'Teaching',
+//     salary:15000,
+//     isHired:false,
+//     heHas:{hand:2,eye:2}
+// };
+// const sd2:User = {
+//     id:2,
+//     name:'Noyon Rahman',
+//     profession:'Software Engineer',
+//     salary:20000,
+//     isHired:true,
+//     heHas:{hand:2,eye:2}
+// };
+// const sd3:User = {
+//     id:3,
+//     name:'Nadim Hassan',
+//     profession:'Civil Engineer',
+//     salary:18000,
+//     isHired:false,
+//     heHas:{hand:2,eye:2}
+// };
 // create a friends array where every array element type is user
-const friends = [sd1, sd2, sd3];
+// const friends: User[] = [sd1, sd2, sd3];
 // 🔍 Accessing Array Elements (Different Ways) 
 // 1. using index : console.log(friends[0]);
 // 2. using slice : console.log(friends.slice(1,2));
@@ -42,16 +76,16 @@ const friends = [sd1, sd2, sd3];
 // 1. push -> end of the array
 // 2. unshift -> first of the array
 // 3. splice -> specific index of array
-const sd4 = {
-    id: 4,
-    name: 'Asraf',
-    profession: 'Mecanical Eng',
-    salary: 10000,
-    isHired: false,
-    heHas: { hand: 2, eye: 2 }
-};
+// const sd4:User = {
+//     id:4,
+//     name:'Asraf',
+//     profession:'Mecanical Eng',
+//     salary:10000,
+//     isHired:false,
+//     heHas:{hand:2,eye:2}
+// };
 // push
-friends.push(sd4);
+// friends.push(sd4);
 // unshift
 // friends.unshift(sd4)
 // splice 
--- typescript-with-w3school/src/4-array.ts
+++ typescript-with-w3school/src/4-array.ts
@@ -1,11 +1,24 @@
 
 /**
- * Array
+ * Array summary
+ * 
  * - An array is a collection of values stored in a single variable.
  * - Only one type → string[], number[]
  * - Or mixed types → using any[], union types, or object[] 
  * 
+ * 🔍 Accessing Array Elements (Different Ways) 
+ * 1. using index : console.log(friends[0]);
+ * 2. using slice : console.log(friends.slice(1,2));
  * 
+ * ➕ Add Elements to Array (Different Ways) 
+ * 1. push -> end of the array
+ * 2. unshift -> first of the array
+ * 3. splice -> specific index of array
+ * 
+ * ➖ Delete Elements from Array (Different Ways) 
+ * 1. pop -> end element of array
+ * 2. shift -> first element of array
+ * 3. splice -> specific index element of array
  *
  *  
 */
@@ -35,44 +48,44 @@
 
 // create object rules using interface
 
-interface User{
-    id:number;
-    name:string;
-    profession:string;
-    salary:number;
-    isHired:boolean;
-    heHas: object;
-}
+// interface User{
+//     id:number;
+//     name:string;
+//     profession:string;
+//     salary:number;
+//     isHired:boolean;
+//     heHas: object;
+// }
 
-const sd1:User = {
-    id:1,
-    name:'Shakil Ahmed',
-    profession:'Teaching',
-    salary:15000,
-    isHired:false,
-    heHas:{hand:2,eye:2}
-};
+// const sd1:User = {
+//     id:1,
+//     name:'Shakil Ahmed',
+//     profession:'Teaching',
+//     salary:15000,
+//     isHired:false,
+//     heHas:{hand:2,eye:2}
+// };
 
-const sd2:User = {
-    id:2,
-    name:'Noyon Rahman',
-    profession:'Software Engineer',
-    salary:20000,
-    isHired:true,
-    heHas:{hand:2,eye:2}
-};
+// const sd2:User = {
+//     id:2,
+//     name:'Noyon Rahman',
+//     profession:'Software Engineer',
+//     salary:20000,
+//     isHired:true,
+//     heHas:{hand:2,eye:2}
+// };
 
-const sd3:User = {
-    id:3,
-    name:'Nadim Hassan',
-    profession:'Civil Engineer',
-    salary:18000,
-    isHired:false,
-    heHas:{hand:2,eye:2}
-};
+// const sd3:User = {
+//     id:3,
+//     name:'Nadim Hassan',
+//     profession:'Civil Engineer',
+//     salary:18000,
+//     isHired:false,
+//     heHas:{hand:2,eye:2}
+// };
 
 // create a friends array where every array element type is user
-const friends: User[] = [sd1, sd2, sd3];
+// const friends: User[] = [sd1, sd2, sd3];
 
 
 // 🔍 Accessing Array Elements (Different Ways) 
@@ -86,17 +99,17 @@ const friends: User[] = [sd1, sd2, sd3];
 // 2. unshift -> first of the array
 // 3. splice -> specific index of array
 
-const sd4:User = {
-    id:4,
-    name:'Asraf',
-    profession:'Mecanical Eng',
-    salary:10000,
-    isHired:false,
-    heHas:{hand:2,eye:2}
-};
+// const sd4:User = {
+//     id:4,
+//     name:'Asraf',
+//     profession:'Mecanical Eng',
+//     salary:10000,
+//     isHired:false,
+//     heHas:{hand:2,eye:2}
+// };
 
 // push
-friends.push(sd4);
+// friends.push(sd4);
 
 // unshift
 // friends.unshift(sd4)
