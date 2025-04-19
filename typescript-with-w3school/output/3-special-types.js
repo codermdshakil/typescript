69f99bb354a22888706577cf364d6f736d464d8a
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-20 00:35:45 +0600

    * Type: any  * ----------  * - When you use any, TypeScript stops checking type safety for that variable.

---
+++ typescript-with-w3school/output/3-special-types.js
@@ -0,0 +1,21 @@
+"use strict";
+/**
+ * Type: any
+ * ----------
+ * - When you use any, TypeScript stops checking type safety for that variable.
+ *
+ * */
+/*  Example without any */
+// let u = 'shakil ahmed';
+// u = 33; //  error 
+// console.log(u);
+/*  Example with any */
+// let firstName :any = 'Shakil Ahmed';
+// firstName = 22;
+// console.log(firstName);
+//  When you are using any it stop checking data type and we can assign any type of data
+// let data:any = 'Shakil Ahmed';
+// data = 20;
+// data = {isHired:false};
+// data = ['first', 'second', 'third'];
+// console.log(data);
