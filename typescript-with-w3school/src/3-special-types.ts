c9ae9bd1d5cc1964739498ea9a68b8aec7a8b0bd
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-20 00:45:28 +0600

    // ## what is the differents between any type and unknown type ?
    // 1. With any type, you can do anything with it without any error.
    // 2. With unknown type, you can't do anything until you check the data type of that variable.
    // 3. So, we can say that unknown is a safer version of any type.

--- typescript-with-w3school/src/3-special-types.ts
+++ typescript-with-w3school/src/3-special-types.ts
@@ -21,13 +21,38 @@
 // console.log(firstName);
 
 
-//  When you are using any it stop checking data type and we can assign any type of data
+// ## When you are using any it stop checking data type and we can assign any type of data
+
 // let data:any = 'Shakil Ahmed';
 // data = 20;
 // data = {isHired:false};
 // data = ['first', 'second', 'third'];
 // console.log(data);
 
+
+// ## Alternative of any is : unknown
+
+// let data: unknown = "hello";
+// data = 34;
+// console.log(typeof data);
+
+// ## what is the differents between any type and unknown type ? 
+
+// 1. With any type, you can do anything with it without any error.
+// 2. With unknown type, you can't do anything until you check the data type of that variable. 
+// 3. So, we can say that unknown is a safer version of any type.
+
+
+// let data: unknown = "hello";
+
+// data.toUpperCase() - error
+
+
+// if( typeof data === 'string'){
+//     console.log(data.toUpperCase()); // Ok
+// }
+
+// when we want to make some operations with unknow type value then we must check it type that's why unknown is safer more then any type
 
 
 
