e03c28841e2b5e1364e42188a884438299d1c2ef
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-19 15:25:11 +0600

    Learn : Simple types in typescript 1. Explicit, 2. Implicit

---
+++ typescript-with-w3school/src/2-simple-types.ts
@@ -0,0 +1,28 @@
+// There are three main primitives in JavaScript and TypeScript
+/**
+ * - boolean - true or false
+ * - number - interger and floating numbers
+ * - string - text values like = 'Hello TypeScript'
+ * 
+ * */ 
+
+/*Type Assignment */
+// 1. Explicit 
+// 2. Implicit
+
+
+// 1. Explicit assignment example
+// const firstName: string = 'Hello world';
+// console.log(firstName);
+
+// 2. Implicit assignment example
+// const firstName = "Dylan";
+// console.log(firstName);
+
+
+// Unable to infer
+const json = JSON.parse("55");
+// console.log(typeof json); // number
+
+
+
