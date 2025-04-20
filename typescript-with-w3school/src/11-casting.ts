6c250137e1bf164bc2174abc12e91b9fc64463b2
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-21 00:42:11 +0600

    * TypeScript Casting (as, <>)  *  * - casting using as keyword  * - casting using <>  *  * Method           | Example                       | Use Case  * --------------------------------------------------------------------------------------  * as syntax        | value as string               | ✅ Safe and preferred in most cases  * ----------------------------------------------------------------------------------------  * <type>           | <string>value                 | ✅ Valid, but not for JSX (React)  * ----------------------------------------------------------------------------------------  * Double cast      | value as unknown as SomeType  | ⚠️ Dangerous — use only when sure  *  *  * when working with types where it's necessary to override the type of a variable, such as when  * incorrect types are provided by a library.  *

---
+++ typescript-with-w3school/src/11-casting.ts
@@ -0,0 +1,33 @@
+
+/**
+ * TypeScript Casting (as, <>)
+ * 
+ * - casting using as keyword
+ * - casting using <>
+ * 
+ * Method           | Example                       | Use Case
+ * --------------------------------------------------------------------------------------
+ * as syntax        | value as string               | ✅ Safe and preferred in most cases
+ * ----------------------------------------------------------------------------------------
+ * <type>           | <string>value                 | ✅ Valid, but not for JSX (React)
+ * ----------------------------------------------------------------------------------------
+ * Double cast      | value as unknown as SomeType  | ⚠️ Dangerous — use only when sure
+ * 
+ * 
+ * when working with types where it's necessary to override the type of a variable, such as when
+ * incorrect types are provided by a library.
+ * 
+*/
+
+
+// Casting using as keyword
+
+const myVar:unknown = "Hello";
+// console.log((myVar as string).length); // 5
+
+// ## Casting using <>
+
+const myVar2: unknown = "Shakil Ahmed";
+// console.log((<string>myVar2).length);
+
+
