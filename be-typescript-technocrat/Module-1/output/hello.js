491f86ebfa7fadf4091ae45a14227f5d2d839b7c
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-22 12:42:38 +0600

    * Project Setup  *  * 1. npm install -g typescript  * 2. tsc --init  * 3. src --> rootDir ( change in tsconfig )  * 4. output --> outDir ( change in tsconfig )

---
+++ be-typescript-technocrat/Module-1/output/hello.js
@@ -0,0 +1,12 @@
+"use strict";
+const myName = "Shakil Ahmed";
+console.log(myName);
+/**
+ * Project Setup
+ *
+ * 1. npm install -g typescript
+ * 2. tsc --init
+ * 3. src --> rootDir ( change in tsconfig )
+ * 4. output --> outDir ( change in tsconfig )
+ *
+ * */
---
+++ be-typescript-technocrat/Module-1/src/hello.ts
@@ -0,0 +1,14 @@
+
+const myName = "Shakil Ahmed";
+console.log(myName);
+
+/**
+ * Project Setup
+ * 
+ * 1. npm install -g typescript
+ * 2. tsc --init
+ * 3. src --> rootDir ( change in tsconfig )
+ * 4. output --> outDir ( change in tsconfig )
+ * 
+ * */ 
+
