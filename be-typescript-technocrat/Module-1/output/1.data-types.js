f3b1d71b8c40d0df9843793b71bb8685b8f977bd
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-22 12:59:25 +0600

    * // Data types  *  * Primitive Data types  *  * - string  * - number  * - boolean  * - undefine  * - null  *  * * Non Primitive Data types  *  * - Array  * - Object  * - Tuple  *

---
+++ be-typescript-technocrat/Module-1/output/1.data-types.js
@@ -0,0 +1,47 @@
+"use strict";
+/**
+ *
+ * // Data types
+ *
+ * Primitive Data types
+ *
+ * - string
+ * - number
+ * - boolean
+ * - undefine
+ * - null
+ *
+ * * Non Primitive Data types
+ *
+ * - Array
+ * - Object
+ * - Tuple
+ *
+*/
+// string
+const firstName = "Shakil Ahmed";
+// number
+const age = 20;
+// boolean
+const isAdult = true;
+// undefine
+const x = undefined;
+// null
+const y = null;
+// any -> disable type check
+/**
+ * Non Primitive Data types
+ *
+ * - Array
+ * - Object
+ * - Tuple
+ *
+*/
+// array
+// const friends: string[] = []; 
+// const friends1: number[] = []; 
+// const friends2 : (number| string | boolean | object)[] = [] 
+// const friends3 : (number| string)[] = [undefine, false, {}];
+// tuple -> array -> order matter -> fixed length -> each element different data type
+// const ageAndName1:[number, string] = [20, 'shakil'];
+// const ageAndName2:[number, string, string] = [20, 'shakil', 'noyon'];
